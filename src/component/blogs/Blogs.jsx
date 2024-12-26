54eb27bbed0ae9935e1eb0d38169ec67a3e7defd
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-26 10:12:11 +0100

    add readme

--- src/component/blogs/Blogs.jsx
+++ src/component/blogs/Blogs.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { gql, useQuery } from "@apollo/client";
 import { GET_BLOGS_INFO } from '../../graphql/queries';
+
 export default function Blogs() {
  const  {loading , data , error}=useQuery(GET_BLOGS_INFO)  
+ if(loading)return <h4>Loading...</h4>
+ if(error)return <h4>error...</h4>
  console.log(data);
- 
   return (
     <div>
-      
+      Blogs
     </div>
   )
 }
