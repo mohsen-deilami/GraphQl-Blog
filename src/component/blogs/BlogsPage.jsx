c4a15edb65fe862ccccdb5d506b7498f3be872c8
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-29 15:44:12 +0100

    develop blogpage css

--- src/component/blogs/BlogsPage.jsx
+++ src/component/blogs/BlogsPage.jsx
@@ -1,87 +1,93 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { GET_POST_INFO } from '../../graphql/queries'
-import { useParams } from "react-router-dom";
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_POST_INFO } from "../../graphql/queries";
+import { useNavigate, useParams } from "react-router-dom";
 import { Container } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import  Loader from './../../component/sheared/Loader';
-import sanitizeHtml from 'sanitize-html';
+import Loader from "./../../component/sheared/Loader";
+import sanitizeHtml from "sanitize-html";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 export default function BlogsPage() {
-    const { slug } = useParams();
-    const { loading, data, error } = useQuery(GET_POST_INFO, {
-      variables: { slug },
-    });
-  console.log(data)
+  const navigate = useNavigate();
+  const { slug } = useParams();
+  const { loading, data, error } = useQuery(GET_POST_INFO, {
+    variables: { slug },
+  });
+  console.log(data);
   return (
-    <Container maxWidth="lg">
-    {!!loading &&  <Loader/>}
-    {!!error &&  <Loader/>}
+    <Container maxWidth="lg" style={{ padding: "0" }}>
+      {!!loading && <Loader />}
+      {!!error && <Loader />}
 
-    {data ? (
-      <Grid container mt={5} mb={5}>
-      
-          <Grid>  
-            <Typography
-            component="h3"
-            variant="h5"
-            sx={{ color: "text.primary", m:"16px", fontWeight: "700" }}
+      {data ? (
+        <Grid container mt={5} mb={5}>
+          <Grid
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              padding: "10px",
+            }}
+            size={{ sm: 12 }}
           >
-            {data.post.title}
-          </Typography>
+            <Typography
+              component="h3"
+              variant="h5"
+              sx={{ color: "text.primary", m: "16px", fontWeight: "700" }}
+            >
+              {data.post.title}
+            </Typography>
+            <ArrowForwardIcon onClick={() => navigate(-1)} />
           </Grid>
-      
 
-        <Grid  sx={{ mt:'10px' }}
-             size={{ xs: 12 }}
-        >
-          <img
-            src={`${data.post.coverPhoto.url}`}
-           style={{width:'100%' }}
-          />
-
-</Grid>
-        <Grid container mt={10} >
-          <Grid sx={{display:'flex' , alignItems:'center' , justifyContent:'center'}}>
-
-         
-        <Avatar
-              src={`${data.post.author.avatar.url}`}
-              sx={{ marginRight: "20px", width: "200px", height: "200px" }}
+          <Grid sx={{ mt: "10px" }} size={{ xs: 12 }}>
+            <img
+              src={`${data.post.coverPhoto.url}`}
+              style={{ width: "100%" }}
             />
-            <Grid>
-            <Typography
-              component="h3"
-              variant="h5"
-              sx={{ color: "text.primary", m:"16px", fontWeight: "700" }}
+          </Grid>
+          <Grid container mt={10}>
+            <Grid
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
             >
-              {data.post.author.name}
-            </Typography>
-            <Typography
-              component="h3"
-              variant="h5"
-              sx={{ color: "text.secondry" }}
-            >
-              {data.post.author.field}
-            </Typography>
+              <Avatar
+                src={`${data.post.author.avatar.url}`}
+                sx={{ marginRight: "25px", width: "150px", height: "150px" }}
+              />
+              <Grid>
+                <Typography
+                  component="h3"
+                  variant="h5"
+                  sx={{ color: "text.primary", fontWeight: "900" }}
+                >
+                  {data.post.author.name}
+                </Typography>
+                <Typography
+                  component="h3"
+                  variant="h5"
+                  sx={{ color: "text.secondry" }}
+                >
+                  {data.post.author.field}
+                </Typography>
+              </Grid>
             </Grid>
-            </Grid>
+          </Grid>
+
+          <Grid size={{ xs: 12 }} mt={10}>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: sanitizeHtml(data.post.content.html),
+              }}
+            ></div>
+          </Grid>
         </Grid>
-
-        
-
-        <Grid size={{ xs: 12 }}mt={10} >
-          <div
-            dangerouslySetInnerHTML={{ __html:sanitizeHtml( data.post.content.html) }}
-          ></div>
-        </Grid>
-      </Grid>
-    ) : null}
-
-  
-   
-  </Container>
-);
-  
+      ) : null}
+    </Container>
+  );
 }
