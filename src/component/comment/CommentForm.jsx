ebd6d0cdf4047c258c9bbc3cf1b9bd6d9b07763f
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2024-12-30 10:03:46 +0100

    add react-toasty to project

--- src/component/comment/CommentForm.jsx
+++ src/component/comment/CommentForm.jsx
@@ -5,13 +5,22 @@ import Typography from "@mui/material/Typography";
 import { TextField } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useMutation } from '@apollo/client';
-
+import { toast, ToastContainer } from 'react-toastify';
 export default function CommentForm(slug) {
   console.log(slug)
     const [name,setName]=useState('');
     const [email,setEmail]=useState('');
     const [text,setText]=useState('');
  const [sendComment ,{loading , data , error}]=useMutation(SEND_COMMENT ,{variables:{name, email, text , slug}});
+ console.log(data)
+ const clickHandler=()=>{
+if(name && email && text){
+  sendComment();
+
+}else{
+toast.warn("Fill in all fields",{position:"top-center"})
+}
+ }
   return (
    <Grid container sx={{boxShadow:"rgba(0,0,0,0.1) 0px 4px 12px" , borderRadius:4 , py:1 , mt:5}}>
     <Grid size={{xs:12}} m={2}>
@@ -20,14 +29,12 @@ export default function CommentForm(slug) {
     <Grid size={{xs:12}} m={2}>
    
     <TextField
-          required
           label="username"
           value={name}
           sx={{width:'100%' , my:2}}
           onChange={e=>setName(e.target.value)}
         />
     <TextField
-          required
           id="outlined-required"
           label="email"
           value={email}
@@ -35,7 +42,6 @@ export default function CommentForm(slug) {
           onChange={e=>setEmail(e.target.value)}
         />
     <TextField
-          required
           id="outlined-required"
           label="text"
           value={text}
@@ -46,7 +52,10 @@ export default function CommentForm(slug) {
         />
     </Grid >
 <Grid sx={{padding:2}}>
-<Button variant="contained">Send Comment</Button>
+  {loading ? <Button variant="contained" disabled>Sendingt</Button> 
+   :
+    <Button variant="contained" onClick={clickHandler}>Send Comment</Button> }
+<ToastContainer/>
 </Grid>
    </Grid>
   )
